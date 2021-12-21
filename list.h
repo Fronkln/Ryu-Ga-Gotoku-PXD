308dd557fda23079528e55445a4865c4f2195009
Jhrino <orhankaymak@windowslive.com>
2021-12-21 18:59:10 +0300

    t_instance_list,  t_instance_list_node_struct

--- list.h
+++ list.h
@@ -23,4 +23,26 @@ namespace pxd
         T *mp_top;
         T *mp_bottom;
     };
+
+    template <typename T>
+    struct t_instance_list_node_struct
+    {
+        T* m_value;
+        void *mp_prev;
+        void *mp_next;
+    };
+
+
+    template <typename T, typename allocator>
+    struct t_instance_list
+    {
+        struct node_t
+        {
+            T* m_value;
+            pxd::t_instance_list<T*, allocator>::node_t *mp_prev;
+            pxd::t_instance_list<T*, allocator>::node_t *mp_next;
+        };
+
+        pxd::t_pointer_list<pxd::t_instance_list<T, allocator>::node_t, pxd::t_pointer_list_linker<pxd::t_instance_list<T, allocator>::node_t>> m_list;
+    };
 }
